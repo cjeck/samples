8fe1aae2c58108b811f9e17ffce9f176f1853fb1
engrsolis <43598306+engrsolis@users.noreply.github.com>
2018-10-26 21:38:47 +0800

    Create (JS)Get Date and Time.js

---
+++ (JS)Get Date and Time.js
@@ -0,0 +1,15 @@
+// Provides Date and Time below
+// On this sample, OS is assumed to be providing military time.
+
+var today = new Date();
+var day = today.getDay();
+var hour = ( -1 < today.getHours() && today.getHours() < 12)? (today.getHours()):((today.getHours())-12);
+var minutes = today.getMinutes();
+var seconds = today.getSeconds();
+var m = (today.getHours() > 12)? " PM":" AM";
+var dayArray = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+function todayIs(){
+console.log('Day today is: ' + dayArray[day]);
+console.log('Current Time is: ' + hour + ':' + minutes + ':' + seconds + "" + m);
+}
